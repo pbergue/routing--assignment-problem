cb49a5fc3fc37023133303822f154c893c98df05
Pierre Bergue <pierre.bergue@gmail.com>
2021-02-18 18:59:16 +0100

    added the link and title in the params

--- src/containers/Course/Course.js
+++ src/containers/Course/Course.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
+import queryString from 'query-string';
 
 class Course extends Component {
-    render () {
-        return (
-            <div>
-                <h1>_COURSE_TITLE_</h1>
-                <p>You selected the Course with ID: _ID_</p>
-            </div>
-        );
-    }
+
+  render () {
+    const search = queryString.parse(this.props.location.search);
+    // const params = new URLSearchParams(search);
+    // const title = params.get('title');
+    // console.log(search, params, title);
+      return (
+          <div>
+              <h1>{search.title}</h1>
+              <p>You selected the Course with ID: {this.props.match.params.id}</p>
+          </div>
+      );
+  }
 }
 
 export default Course;
--- src/containers/Courses/Courses.js
+++ src/containers/Courses/Courses.js
@@ -1,30 +1,39 @@
 import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+// import Course from '../Course/Course';
 
 import './Courses.css';
 
-class Courses extends Component {
-    state = {
-        courses: [
-            { id: 1, title: 'Angular - The Complete Guide' },
-            { id: 2, title: 'Vue - The Complete Guide' },
-            { id: 3, title: 'PWA - The Complete Guide' }
-        ]
-    }
+const courses = props => {
+    const coursesList = [
+        { id: 1, title: 'Angular - The Complete Guide' },
+        { id: 2, title: 'Vue - The Complete Guide' },
+        { id: 3, title: 'PWA - The Complete Guide' }
+    ];
 
-    render () {
-        return (
-            <div>
-                <h1>Amazing Udemy Courses</h1>
-                <section className="Courses">
-                    {
-                        this.state.courses.map( course => {
-                            return <article className="Course" key={course.id}>{course.title}</article>;
-                        } )
-                    }
-                </section>
-            </div>
-        );
-    }
+    return (
+      <div>
+          <h1>Amazing Udemy Courses</h1>
+          <section className="Courses">
+              {
+                  coursesList.map( course => {
+                      return (
+                        <Link
+                          to={{
+                            pathname: `/courses/${course.id}`,
+                            search: `&title=${course.title}`
+                          }}
+                          key={course.id}
+                          id={course.id}>
+                          {/* <Course className="Course">{course.title}</Course>; */}
+                          {course.title}
+                        </Link>
+                      );
+                  } )
+              }
+          </section>
+      </div>
+    );
 }
 
-export default Courses;
+export default courses;
